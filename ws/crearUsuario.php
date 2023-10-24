7188fc279a387fcf675990c246f92c812dec61e4
Francisco Rafael Mellado Martínez <frmm345@gmail.com>
2023-10-24 12:06:50 +0200

    Falta almacenar datos en un archivo

--- ws/crearUsuario.php
+++ ws/crearUsuario.php
@@ -1,10 +1,22 @@
 <?php
 
+include ("models/User.php");
+
 $nombre = isset($_POST['nombre'])? $_POST['nombre'] : 'nombre';
 $apellidos = isset($_POST['apellidos'])? $_POST['apellidos'] : 'apellidos';
 $password = isset($_POST['password'])? $_POST['password'] : 'password';
 $telefono = isset($_POST['telefono'])? $_POST['telefono'] : 'telefono';
-$nombre = isset($_POST['email'])? $_POST['email'] : 'email';
-$nombre = isset($_POST['sexo'])? $_POST['sexo'] : 'sexo';
+$email = isset($_POST['email'])? $_POST['email'] : 'email';
+$sexo = isset($_POST['sexo'])? $_POST['sexo'] : 'sexo';
+
+$user = new User();
+$user -> setNombre($nombre);
+$user -> setApellidos($apellidos);
+$user -> setPassword($password);
+$user -> setTelefono($telefono);
+$user -> setEmail($email);
+$user -> setSexo($sexo);
+
+echo $user -> toJson();
 
 ?>
