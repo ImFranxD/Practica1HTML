fed41ba029347ed7b3f8be620a22a1a1bbf9cde1
Francisco Rafael Mellado Martínez <frmm345@gmail.com>
2023-11-16 10:46:24 +0100

    Tabla arreglada, el error estaba en haber utilizado windows.onload en 2 archivos

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,7 @@
-window.onload = function(){
+//Uso windows.addEventListener porque en scripts.js tengo un windows.onload y eso causa un conflicto entre ambos archivos haciendo que uno de los 2 no funcione
+window.addEventListener('load', function() {
     fetch('nav.html').then(response => response.text()).then(data => {
         document.getElementById('contenedorNav').innerHTML = data;
     }).catch(error => console.error("Error al cargar el nav: ", error));
-};
+});
+    
