05304548f93afd58948ba7033d84c3c3cdb04032
Francisco Rafael Mellado Martínez <frmm345@gmail.com>
2023-11-17 11:23:27 +0100

    Técnicamente deberia cambiar de color, ¿lo hace? no

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,21 @@
 //Uso windows.addEventListener porque en scripts.js tengo un windows.onload y eso causa un conflicto entre ambos archivos haciendo que uno de los 2 no funcione
 window.addEventListener('load', function() {
+
+    var rutaActual = window.location.pathname;
+    var contenedorNav = document.getElementById('contenedorNav');
+
     fetch('nav.html').then(response => response.text()).then(data => {
         document.getElementById('contenedorNav').innerHTML = data;
+
+        var navLinks = contenedorNav.querySelectorAll('ul li a');
+
+        navLinks.forEach(function(enlace){
+            console.log(enlace.getAttribute('href'));
+            if(rutaActual.includes(enlace.getAttribute('href'))){
+                enlace.classList.add('active');
+                console.log("Entro al if", enlace);
+            }
+        });
     }).catch(error => console.error("Error al cargar el nav: ", error));
 });
     
