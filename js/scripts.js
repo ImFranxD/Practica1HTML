fae2110a8e062114d2f4337d344eb2e325eae27c
Francisco Rafael Mellado Martínez <frmm345@gmail.com>
2023-12-06 13:09:12 +0100

    Llamada asincrona hecha en getUsuarios

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,58 @@
 window.onload = function() {
 
+    /*
     const usuarios = [
         {nombre : "Francisco Rafael", apellidos : "Mellado Martínez", email : "frmm345@gmail.com", tlf : 123456789},
         {nombre : "Juan", apellidos : "López Botella", email : "prueba@prueba.com", tlf : 123456789},
         {nombre : "Guillermo", apellidos : "Canales Albert", email : "prueba123@prueba123.com", tlf : 123456789},
         {nombre : "Samuel", apellidos : "Llanos Gilbert", email : "prueba456@prueba456.com", tlf : 123456789}
-    ];
+    ];*/
 
+    cargarAlumnos();
+    
     const cuerpoTabla = document.querySelector("table tbody");
 
+    async function cargarAlumnos(){
+        try{
+            const response = await fetch('ws/getUsuario.php');
+            const data = await response.json();
+
+            console.log("Entro dentro de la funcion");
+
+            if (data.success){
+                console.log("He conseguido los datos");
+                const alumno = data.data;
+                rellenarTabla(alumno);
+            }
+        }catch(error){
+            console.error("Error en la solicitud: " + error.message);
+        }
+    }
+
+    function rellenarTabla(alumnos){
+        console.log("Entro a rellenar tabla");
+
+        cuerpoTabla.innerHTML = "";
+        if(Array.isArray(alumnos)){
+            alumnos.forEach((alumno, index) => {
+                const row = cuerpoTabla.insertRow();
+                row.innerHTML = `<td>${alumno.nombre}</td><td>${alumno.apellidos}</td><td>${alumno.password}</td><td>${alumno.telefono}</td><td>${alumno.email}</td><td>${alumno.sexo}</td><td>${alumno.fecha_nacimiento}</td><td><button class="borrarUsuario" data-index="${index}">X</button><button class="editarUsuario" data-index="${index}" onclick="editarUsuario('${index}')">Editar</button></td>`;
+            });
+        }else{
+            console.error("No es un array");
+        }
+        
+    }
+
+    
+
+    /*Antiguo rellenarTabla
     function rellenarTabla(){
         cuerpoTabla.innerHTML = "";
         usuarios.forEach((usuario, index) => {
             const row = cuerpoTabla.insertRow();
             row.innerHTML = `<td>${usuario.nombre}</td><td>${usuario.apellidos}</td><td>${usuario.email}</td><td>${usuario.tlf}</td><td><button class="borrarUsuario" data-index="${index}">X</button><button class="editarUsuario" data-index="${index}" onclick="editarUsuario('${index}')">Editar</button></td>`;        });
-    }
+    }*/
 
     function filtrarTabla(){
         const input = document.getElementById("filtro");
